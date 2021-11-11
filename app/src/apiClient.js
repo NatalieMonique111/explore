b130bf2f2819043d075caa341a58ab4251429a2a
NatalieMonique111 <nataliemonique111@gmail.com>
2021-11-11 09:03:15 -0800

    removed dead code: the post request

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -3,17 +3,3 @@ export const getRegion = (region) => _get(`/api/region/${region}`);
 export const getSite = (site) => _get(`/api/site/${site}`);
 
 const _get = async (url) => (await fetch(url)).json();
-
-const _post = async (url, body) => {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
-  });
-  let result;
-  try {
-    result = await response.json();
-  } catch {}
-
-  return result;
-};
